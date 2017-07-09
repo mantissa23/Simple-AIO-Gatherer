887003a901e23c0bf6ad7deece7578087d17a1ed
demmonic <evsubiw@gmail.com>
2017-07-09 01:46:48 -0400

    non retard banking

--- Albion Gathering Script/BankState.cs
+++ Albion Gathering Script/BankState.cs
@@ -21,67 +21,70 @@ namespace Ennui.Script.Official
         public override int OnLoop(IScriptEngine se)
         {
             var localPlayer = Players.LocalPlayer;
-            if (localPlayer != null)
+            if (localPlayer == null)
             {
-                if (!config.VaultArea.Contains(localPlayer.ThreadSafeLocation))
-                {
-                    var config = new PointPathFindConfig();
-                    config.ClusterName = this.config.CityClusterName;
-                    config.UseWeb = false;
-                    config.Point = this.config.VaultDest;
-                    
-                    Movement.PathFindTo(config);
-                    return 0;
-                }
-                
-                if (config.FleeOnLowHealth && localPlayer.HealthPercentage <= config.FleeHealthPercent)
-                {
-                    return 1000;
-                }
-                
-                if (!Banking.IsOpen)
-                {
-                    var bank = Objects.BankChain.Closest(localPlayer.ThreadSafeLocation);
-                    if (bank == null)
-                    {
-                        Logging.Log("failed to find bank vault building");
-                        return 5000;
-                    }
+                Logging.Log("Failed to find local player!", LogLevel.Warning);
+                return 10_000;
+            }
 
-                    bank.Click();
-                    Time.SleepUntil(() =>
-                    {
-                        return Banking.IsOpen;
-                    }, 4000);
-                }
-                
-                if (Banking.IsOpen)
+            if (!config.VaultArea.Contains(localPlayer.ThreadSafeLocation))
+            {
+                var config = new PointPathFindConfig();
+                config.ClusterName = this.config.CityClusterName;
+                config.UseWeb = false;
+                config.Point = this.config.VaultDest;
+
+                Movement.PathFindTo(config);
+                return 0;
+            }
+
+            if (config.FleeOnLowHealth && localPlayer.HealthPercentage <= config.FleeHealthPercent)
+            {
+                return 1000;
+            }
+
+            if (!Banking.IsOpen)
+            {
+                var bank = Objects.BankChain.Closest(localPlayer.ThreadSafeLocation);
+                if (bank == null)
                 {
-                    var beginWeight = localPlayer.WeighedDownPercent;
-                    var allItems = Inventory.GetItemsBySubstring("_ROCK", "_ORE", "_HIDE", "_WOOD", "_FIBER");
-                    var toDeposit = new List<IItemStack>();
-                    foreach (var stack in allItems)
+                    Logging.Log("failed to find bank vault building");
+                    return 5000;
+                }
+
+                bank.Click();
+                Time.SleepUntil(() =>
+                {
+                    return Banking.IsOpen;
+                }, 4000);
+            }
+
+            if (Banking.IsOpen)
+            {
+                var beginWeight = localPlayer.WeighedDownPercent;
+                var allItems = Inventory.GetItemsBySubstring("_ROCK", "_ORE", "_HIDE", "_WOOD", "_FIBER");
+                var toDeposit = new List<IItemStack>();
+                foreach (var stack in allItems)
+                {
+                    if (stack.UniqueName.Contains("JOURNAL"))
                     {
-                        if (stack.UniqueName.Contains("JOURNAL"))
-                        {
-                            toDeposit.Add(stack);
-                        }
+                        toDeposit.Add(stack);
                     }
-                    
-                    if (toDeposit.Count == 0 || Banking.Deposit(toDeposit.ToArray()))
+                }
+
+                if (toDeposit.Count == 0 || Banking.Deposit(toDeposit.ToArray()))
+                {
+                    if (localPlayer.TotalHoldWeight < 99)
                     {
-                        if (localPlayer.TotalHoldWeight < 99)
+                        if (Inventory.HasBrokenItems(70))
                         {
-                            if (Inventory.HasBrokenItems(70))
-                            {
-                                parent.EnterState("repair");
-                            }
-                            else
-                            {
-                                parent.EnterState("gather");
-                            }
-                            return 0;
+                            parent.EnterState("repair");
                         }
+                        else
+                        {
+                            parent.EnterState("gather");
+                        }
+                        return 0;
                     }
                 }
             }
