6ef91d1806c9f87d318d3d6b8cf78ccfd7bb0ed5
demmonic <evsubiw@gmail.com>
2017-07-08 23:27:12 -0400

    assemblyinfo

--- Albion Gathering Script/Properties/AssemblyInfo.cs
+++ Albion Gathering Script/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Albion Gathering Script")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Gathers resources for you using Ennui's Albion Online bot")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ennui LLC")]
 [assembly: AssemblyProduct("Albion Gathering Script")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Ennui LLC 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
